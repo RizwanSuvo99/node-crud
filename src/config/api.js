75567f5f62b4bb53c1c18b64576191b8ef7a405a
rizwansuvo99 <rizwanshuvo@gmail.com>
2025-07-12 02:58:05 +0600

    Node crud implemented

---
+++ src/config/api.js
@@ -0,0 +1,11 @@
+// API Configuration
+const API_CONFIG = {
+  baseURL: 'http://localhost:8080/api',
+  endpoints: {
+    contacts: '/contacts',
+    contact: (id) => `/contacts/${id}`,
+  },
+  timeout: 10000,
+};
+
+export default API_CONFIG;
